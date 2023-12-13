fb8ab50d0bb73e05e714c61d008c3653cd22af79
Minli Zheng <minlizml77@gmail.com>
2023-12-13 15:39:00 -0500

    bmi_10_round

--- roster.py
+++ roster.py
@@ -2,9 +2,19 @@
 
 import pandas as pd
 
-player={"Last Name": ["Bacot","Davis","Cadeau"],
-        "First Name":["Armando","RJ","Elliot"],
-        "height":[83,72,73],
-        "weight":[240,180,180]}
+player={"Last Name": ["Bacot","Davis","Cadeau","Wojcik","Farris","Lebo","Landry","Okonkwo","Ingram","Trimble"],
+        "First Name":["Armando","RJ","Elliot","Paxson","Duwe","Creighton","Rob","James","Harrison","Seth"],
+        "height":[83,72,73,65,67,61,64,68,67,63],
+        "weight":[240,180,180,195,210,180,190,240,235,195]}
+
 data=pd.DataFrame(player)
+
+#bmi = weight in kg/ height in meters squared
+data["bmi"]=(data["weight"]/2.205)/((data["height"]/39.37)**2)
+
+data["bmi"] = data["bmi"].round(2)
+
 print(data)
+
+data.to_csv("bmi.csv")
+
